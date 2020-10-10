ed5b3d0df6ab097eb31371f7ed49261cdcc8308c
Shilpaj <shilpajbhalerao@gmail.com>
2020-10-10 17:28:09 +0530

    Spawn Model using code

--- nodes/Models.py
+++ nodes/Models.py
@@ -10,41 +10,46 @@ import time
 from turtlesim.srv import *
 from std_srvs.srv import Empty
 from gazebo_msgs.srv import SpawnModel, DeleteModel
+from gazebo_msgs.msg import *
+from geometry_msgs.msg import *
 
 
 class Models:
-    def __init__(self, name):
+    def __init__(self, file, name, position, orientation, ref_frame):
+        self._filename = file
         self._name = name
+        self._x_pos = position[0]
+        self._y_pos = position[1]
+        self._z_pos = position[2]
+        self._roll = orientation[0]
+        self._pitch = orientation[1]
+        self._yaw = orientation[2]
+        self._ref = ref_frame
 
-        rospy.Subscriber('/lunar_world/set_link_state', gazebo_msgs/LinkState, self.callback_link_states)
-        rospy.Subscriber('/lunar_world/set_model_state', gazebo_msgs/ModelState, self.callback_model_states)
 
-        while not rospy.is_shutdown():
-            self.pause()
-            time.sleep(5)
-            self.unpause()
-            time.sleep(5)
-            self.reset_world()
-            time.sleep(5)
-            self.reset()
-        exit()
-
-    # def callback_link_states(self):
-    #     pass
-    #
-    # def callback_model_states(self):
-    #     pass
-    #
-    # def use_sim_time(self):
-    #     rospy.set_param('/use_sim_time', True)
-
-    def spawn_urdf(self):
+    def spawn(self):
         """
-            Function to Reset the Simulator
+        Function to Reset the Simulator
         """
+        pos = Pose()
+        coor = Point()
+        rot = Quaternion()
+
+        coor.x = self._x_pos
+        coor.y = self._y_pos
+        coor.z = self._z_pos
+
+        rot.x = self._roll
+        rot.y = self._pitch
+        rot.z = self._yaw
+        rot.w = 0.0
+
+        pos.position = coor
+        pos.orientation = rot
+
         try:
-            reset_serv = rospy.ServiceProxy('/lunar_world/spawn_urdf_model', SpawnModel)
-            reset_serv()
+            reset_serv = rospy.ServiceProxy('/lunar_world/spawn_sdf_model', SpawnModel)
+            reset_serv(self._name, self._filename, '', pos, self._ref)
         except rospy.ServiceException as e:
             rospy.loginfo("Service execution failed: %s" + str(e))
 
@@ -74,6 +79,13 @@ class Models:
 
 if __name__ == '__main__':
     try:
-        sim = Models(robot)
+        filename = '/home/shilpaj/ROS_ws/src/sim/model/husky/model.sdf'
+        name = 'husk'
+        position = [0, 0, 0]
+        orientation = [0, 0, 0]
+        ref_frame = 'world'
+
+        husky = Models(filename, name, position, orientation, ref_frame)
+        husky.spawn()
     except KeyboardInterrupt:
         exit()
