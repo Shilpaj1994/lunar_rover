bbf80004d8d0f4d3372b85af891b4ec9fce5f731
Shilpaj <shilpajbhalerao@gmail.com>
2020-10-12 00:16:08 +0530

    Added example code for gazebo-ros-bridge

---
+++ gazebo_ros_bridge/build/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/listener.cc.o"
+  "listener"
+  "listener.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
