c9af592efa8a57b2d36d2bd7c57abcd20ebc7dfc
Shilpaj <shilpajbhalerao@gmail.com>
2020-10-16 00:55:30 +0530

    Created Python script using Pygazebo library to access sensor data

---
+++ gazebo_ros_bridge/Python/lidar_data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.8
+"""
+Code for accessing lidar data from Gazebo and publish it on ROS topic
+Author: Shilpaj Bhalerao
+Date: Oct 16, 2020
+"""
+
+import sys
+from trollius import From
+import pygazebo
+import pygazebo.msg.laserscan_stamped_pb2 as scan
+
+
+def callback(data):
+    message = scan(data)
+    print('Received message:', message.data)
+
+
+def main():
+    manager = yield From(pygazebo.connect(('localhost', 11345)))
+
+    manager.subscribe('/gazebo/default/hokuyo/link/laser/scan',
+                      'gazebo.msgs.LaserScanStamped',
+                      callback)
+
+
+if __name__ == '__main__':
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        sys.exit()
+        
+
+
+
+
